043b876554af5fde8ad22cf956b1da36a7f1005a
Aaron VonderHaar <av@noredink.com>
2019-06-17 20:41:58 -0700

    Set up elm-test

---
+++ tests/PairingHeapTest.elm
@@ -0,0 +1,11 @@
+module PairingHeapTest exposing (all)
+
+import Expect exposing (Expectation)
+import Test exposing (..)
+
+
+all : Test
+all =
+    test "TODO" <|
+        \() ->
+            Expect.pass
