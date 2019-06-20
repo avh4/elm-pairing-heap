2f92cec50690da6a422a149b45ecba6528e436eb
Aaron VonderHaar <gruen0aermel@gmail.com>
2019-06-19 20:40:45 -0700

    Failing fuzz test

--- tests/PairingHeapTest.elm
+++ tests/PairingHeapTest.elm
@@ -1,11 +1,19 @@
 module PairingHeapTest exposing (all)
 
 import Expect exposing (Expectation)
+import Fuzz
+import PairingHeap
 import Test exposing (..)
 
 
 all : Test
 all =
-    test "TODO" <|
-        \() ->
-            Expect.pass
+    describe "PairingHeap"
+        [ fuzz (Fuzz.list (Fuzz.map2 Tuple.pair Fuzz.int Fuzz.string))
+            "stores and sorts key value pairs"
+          <|
+            \items ->
+                PairingHeap.fromList items
+                    |> PairingHeap.toSortedList
+                    |> Expect.equal (List.sortBy Tuple.first items)
+        ]
